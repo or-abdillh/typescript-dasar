ccc37bfb30f2fdb2771e4c8a16c5b2892af22c3c
or-abdillh <hans.abdillh05@gmail.com>
2023-09-01 09:45:12 +0800

    Part(Type): Object type

---
+++ tests/object-type.test.ts
@@ -0,0 +1,14 @@
+describe('Object', () => {
+
+    it('Should be Object Type', () => {
+
+        const person: { name: string, age: number } = {
+            name: 'Eko',
+            age: 67
+        }
+
+        console.info(person)
+
+        // person.address = 'Java'
+    })
+})
